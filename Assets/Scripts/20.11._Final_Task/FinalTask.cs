65d2f57bf96dcd83ef9304ee8b16a187023e83b3
Max Einolander <91366233+max1m1l1an123@users.noreply.github.com>
2023-11-26 18:51:46 +0200

    final task started
    met requirements:
    
           Scriptin spesifikaatio
    Scriptillä on 2 vaihetta, update1 ja update2. Scriptin kuuluisi voida vaihdella niiden välillä.
    Scripti voi vaihtaa inspectorissa dropdown valikon avulla aloitus seediä.
    [NOT YET] Scriptin kuuluisi päivittää ulkoisen planen materiaalia vaihtamalla sen tekstuuria.
    [NOT YET] Simuloitavan solun väriä tulisi voida muuttaa ennen simulaation aloitusta.
    
    Scriptin tulisi aluksi, kun peli käynnistetään, luoda 2 RenderTexturea default LDR asetuksilla, point filterillä ja enableRandomWrite = true.
    Tekstuurien koko on 512x512.
    Sen tulisi myös löytää Compute Shaderin kaikki kernelit tässä vaiheessa.
    [NOT YET] Compute Shaderiin tulisi liittää tässä vaiheessa kaikkiin kerneleihin tarvittavat tekstuurit ja muut fieldit jotka eivät päivity suorituksen aikana.
    [NOT YET] Scriptin tulisi myös tässä vaiheessa päättää, millä aloitus seedillä simulaatio aloitetaan, ja initialisoida simulaatio.
    
    Pelin ollessa käynnissä, scriptin tulisi ylläpitää aikaväliä, jona simulaatiota päivitetään, eli kuinka nopeasti uusi generaatio tapahtuu.
    [NOT YET] Scriptin tulisi myös pitää kirjaa, kummassa vaiheessa simulaatiota ollaan, ja päättää sen mukaan, kumpi vaihe suorittaa.
    [NOT YET] Scriptin kuuluisi myös päivittää esitykseen käytetyn materiaalin tekstuuria vaiheen mukaan (flipbook).
    
    Kun scripti tuhotaan tai disabloidaan, sen kuuluisi vapauttaa render texture muuttujat muistista.
    
          Compute Shaderin spesifikaatio
    Compute Shaderin tulisi sisältää kaksi kerneliä Update1 ja Update2, jotka päivittävät simulaatiota vuorotellen.
    Shaderin tulisi myös sisältää kernel jokaiselle seedille jota scriptissä voi valita aloitukseen.
    Shaderin tulisi sisältää funktio Game of Lifen solun päivitykselle.

--- Assets/Scripts/20.11._Final_Task/FinalTask.cs
+++ Assets/Scripts/20.11._Final_Task/FinalTask.cs
@@ -7,31 +7,78 @@ public class FinalTask : MonoBehaviour
 {
     [SerializeField] private ComputeShader finalTaskShader;
     [SerializeField] private Material finalTaskMaterial;
+    
     [SerializeField] private RenderTexture blackUVTexture;
     [SerializeField] private RenderTexture whiteUVTexture;
-    
+
     [SerializeField] private int seed = 10;
     [SerializeField] private float updateIntervalSeconds = 2f;
     [SerializeField] private Stage currentStage;
+
     private enum Stage
     {
         Stage1,
         Stage2
     }
-    
+
     private static int _mainKernel;
+    private static int _update1Kernel;
+    private static int _update2Kernel;
+    private static int _forEverySeedKernel;
+
     private static readonly int BaseMap = Shader.PropertyToID("_BaseMap");
     private static readonly int UVMap = Shader.PropertyToID("UVMap");
-    
+
+    public GameObject TileObject;
+    private static readonly int Width = 10;
+    private static readonly int Height = 10;
+    bool[,] grid = new bool[Width, Height];
+    GameObject[,] tiles = new GameObject[Width, Height];
+
     private void Start()
     {
+        #region Generate tiles
+        for (int x = 0; x < Width; x++)
+        {
+            for (int y = 0; y < Height; y++)
+            {
+                // Clear the grid
+                grid[x, y] = false;
+                // Instantiate the tile
+                tiles[x, y] = Instantiate(TileObject,
+                    new Vector3(x, 0f, y) * 1.05f,
+                    TileObject.transform.rotation);
+            }
+        }
+        #endregion Generate tiles
+
+        int textureWidth = 512;
+        int textureHeight = 512;
         // create 2 RenderTextures using default LDR settings, point filter and enableRandomWrite = true.
-        
+        // Create black UV texture
+        blackUVTexture = new RenderTexture(textureWidth, textureHeight, 0, RenderTextureFormat.ARGB32);
+        blackUVTexture.enableRandomWrite = true;
+        blackUVTexture.filterMode = FilterMode.Point;
+        blackUVTexture.Create();
+
+        // Create white UV texture
+        whiteUVTexture = new RenderTexture(textureWidth, textureHeight, 0, RenderTextureFormat.ARGB32);
+        whiteUVTexture.enableRandomWrite = true;
+        whiteUVTexture.filterMode = FilterMode.Point;
+        whiteUVTexture.Create();
+
         // find all kernels of Compute Shader
-        
+        _mainKernel = finalTaskShader.FindKernel("CSMain");
+        _update1Kernel = finalTaskShader.FindKernel("Update1");
+        _update2Kernel = finalTaskShader.FindKernel("Update2");
+        _forEverySeedKernel = finalTaskShader.FindKernel("ForEverySeed");
+
         // attach all kernels of Compute Shader the required textures and other fields that won't be updated during Update
-        
-        // initialize the simulation using seed variable
+        finalTaskShader.SetTexture(_mainKernel, BaseMap, finalTaskMaterial.mainTexture);
+        finalTaskShader.SetTexture(_mainKernel, UVMap, blackUVTexture);
+
+        // TODO: initialize the simulation using seed variable
+        InitializeSimulation(seed);
     }
 
     private void Update()
@@ -39,25 +86,117 @@ public class FinalTask : MonoBehaviour
         // maintain the time interval used to update the simulation (how fast a new generation happens)
         updateIntervalSeconds += Time.deltaTime;
 
-        // keep account on whether the simulation is at Stage1 or Stage2 stage and choose the according stage to execute
+        // TODO: keep account on whether the simulation is at Stage1 or Stage2 stage and choose the according stage to execute
+
+        // TODO: keep track of which Stage of the simulation we are and use the current stage to decide the stage to complete
+
+        // TODO: update the textures of the finalTaskMaterial based on the current stage (flipbook).
         
-        // Scriptin tulisi pitää kirjaa, kummassa vaiheessa simulaatiota ollaan, ja päättää sen mukaan, kumpi vaihe suorittaa.
 
-        // Scriptin kuuluisi päivittää esitykseen käytetyn materiaalin tekstuuria vaiheen mukaan (flipbook).
-    }
+        // use the time interval to do the checks 
+        if (updateIntervalSeconds > 2.0f)
+        {
+            // RULES:
+            // 1. Fewer than 2 live neighbours --> die
+            // 2. 2 or 3 live neighbours -> live on
+            // 3. more than 3 live neighbours -> die
+            // 4. dead cell with 3 live neighbours -> rebirth 
+            for (int x = 0; x < Width; x++)
+            {
+                for (int y = 0; y < Height; y++)
+                {
+                    int live = GetLiveNeighbours(x, y);
+                    if (live < 2)
+                    {
+                        grid[x, y] = false;
+                    }
+                    else if (live < 4 && grid[x, y] == true)
+                    {
+                        // live on... do nothing
+                    }
+                    else if (live > 3 && grid[x, y] == true)
+                    {
+                        grid[x, y] = false;
+                    }
+                    else if (live == 3 && grid[x, y] == false)
+                    {
+                        grid[x, y] = true;
+                    }
+                }
+            }
 
-    // TEHTY:
-    #region Kun scripti tuhotaan tai disabloidaan, sen kuuluisi vapauttaa render texture muuttujat muistista.
-    private void OnDisable()
-    {
-        blackUVTexture.Release();
-        whiteUVTexture.Release();
+            // Update the tiles
+            for (int x = 0; x < Width; x++)
+            {
+                for (int y = 0; y < Height; y++)
+                {
+                    if (grid[x, y] == true)
+                    {
+                        tiles[x, y].GetComponent<MeshRenderer>().material.color = Color.red;
+                    }
+                    else
+                    {
+                        tiles[x, y].GetComponent<MeshRenderer>().material.color = Color.black;
+                    }
+                }
+            }
+            updateIntervalSeconds = 0.0f;
+        }
     }
-    private void OnDestroy()
-    {
-        blackUVTexture.Release();
-        whiteUVTexture.Release();
-    }
-    #endregion Kun scripti tuhotaan tai disabloidaan, sen kuuluisi vapauttaa render texture muuttujat muistista.
     
+    private int GetLiveNeighbours(int x, int y)
+    {
+        int liveneighbours = 0;
+        for (int i = x - 1; i <= x + 1; i++)
+        {
+            for (int j = y - 1; j <= y + 1; j++)
+            {
+                if (!(i == x & j == y) && i >= 0 && j >= 0 && i < Width && j < Height)
+                {
+                    // current i,j is not x,y
+                    if (grid[i, j] == true)
+                    {
+                        liveneighbours++;
+                    }
+                }
+            }
+        }
+        return liveneighbours;
+    }
+
+    // TODO: initialize the simulation using seed variable 
+    void InitializeSimulation(int seed)
+    {
+        // grid[5, 5] = true;
+        // tiles[5, 5].GetComponent<MeshRenderer>().material.color = Color.red;
+        // grid[3, 5] = true;
+        // tiles[3, 5].GetComponent<MeshRenderer>().material.color = Color.red;
+        // grid[4, 4] = true;
+        // tiles[4, 4].GetComponent<MeshRenderer>().material.color = Color.red;
+        // grid[1, 4] = true;
+        // tiles[1, 4].GetComponent<MeshRenderer>().material.color = Color.red;
+        // grid[2, 3] = true;
+        // tiles[2, 3].GetComponent<MeshRenderer>().material.color = Color.red;
+        // grid[1, 3] = true;
+        // tiles[1, 3].GetComponent<MeshRenderer>().material.color = Color.red;
+        // grid[2, 2] = true;
+        // tiles[2, 2].GetComponent<MeshRenderer>().material.color = Color.red;
+    }
+    
+    // DONE:
+    #region when the script is destroyed or disabled, release render texture variables from memory
+
+    void OnDisable()
+    {
+        blackUVTexture.Release();
+        whiteUVTexture.Release();
+    }
+
+    void OnDestroy()
+    {
+        blackUVTexture.Release();
+        whiteUVTexture.Release();
+    }
+
+    #endregion when the script is destroyed or disabled, release render texture variables from memory
 }
